26b06e07a1767e48b7bfca0a2b50bff1d8b95bb3
Amir Mustafa <amirengg15@gmail.com>
2022-10-27 12:45:42 +0530

    feat: type orm database connection with Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mediumclone',
+      password: '8170954991',
+      database: 'mediumclone',
+    }),
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
